9b537980df780ffae5084efa567e03f29b10ba45
originalgremlin <barry.shapira@gmail.com>
2012-10-18 17:44:18 +0300

    Update Android/SoftKeyboard/softkeyboard.js
    Updated for PhoneGap 2.1.0.

--- Android/SoftKeyboard/softkeyboard.js
+++ Android/SoftKeyboard/softkeyboard.js
@@ -1,33 +1,27 @@
-function SoftKeyBoard() {}
+cordova.plugins = cordova.plugins || {};
 
-SoftKeyBoard.prototype.show = function(win, fail) {
-    return PhoneGap.exec(
-            function (args) { if(win !== undefined) { win(args); } }, 
-            function (args) { if(fail !== undefined) { fail(args); } }, 
-            "SoftKeyBoard", 
-            "show", 
-            []);	
+cordova.plugins.SoftKeyBoard = {
+    show: function (win, fail) {
+        return cordova.exec(
+            function (args) { if (win !== undefined) { win(args); } },
+            function (args) { if (fail !== undefined) { fail(args); } },
+            'SoftKeyBoard', 'show', []
+        );
+    },
+
+    hide: function (win, fail) {
+        return cordova.exec(
+            function (args) { if (win !== undefined) { win(args); } },
+            function (args) { if (fail !== undefined) { fail(args); } },
+            'SoftKeyBoard', 'hide', []
+        );
+    },
+
+    isShowing: function (win, fail) {
+        return cordova.exec(
+            function (args) { if (win !== undefined) { win(args); } },
+            function (args) { if (fail !== undefined) { fail(args); } },
+            'SoftKeyBoard', 'isShowing', []
+        );
+    }
 };
-
-SoftKeyBoard.prototype.hide = function(win, fail) {
-    return PhoneGap.exec(
-            function (args) { if(win !== undefined) { win(args); } }, 
-            function (args) { if(fail !== undefined) { fail(args); } },
-            "SoftKeyBoard", 
-            "hide", 
-            []);	
-};
-
-SoftKeyBoard.prototype.isShowing = function(win, fail) {
-    return PhoneGap.exec(
-            function (args) { if(win !== undefined) { win(args); } }, 
-            function (args) { if(fail !== undefined) { fail(args); } },
-            "SoftKeyBoard", 
-            "isShowing", 
-            []);	
-};
-
-PhoneGap.addConstructor(function() {
-    PhoneGap.addPlugin('SoftKeyBoard', new SoftKeyBoard());
-    PluginManager.addService("SoftKeyBoard","com.zenexity.SoftKeyBoardPlugin.SoftKeyBoard");
-});
